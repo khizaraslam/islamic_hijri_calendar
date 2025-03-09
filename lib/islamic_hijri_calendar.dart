bf68ecc6c82ef5203ec3b775584ec1e7c219fda9
khizaraslam <khizaraslam201@gmail.com>
2025-03-09 11:21:08 +0500

    Change in date function

--- lib/islamic_hijri_calendar.dart
+++ lib/islamic_hijri_calendar.dart
@@ -4,6 +4,6 @@ export 'src/islamic_hijri_calendar.dart';
 export 'src/hijri_view_model.dart';
 export 'src/hijri_calendar_config.dart';
 export 'package:intl/intl.dart';
-export 'date_functions.dart';
+export 'src/date_functions.dart';
 
 
